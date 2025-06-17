559baf700483efcf22b67e37e8d0d18fbe99ba5a
Ahmed Nofal <anofal@digiarenas.com>
2025-06-17 14:38:09 +0300

    fix configration

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment.prod';
+import { environment } from './environments/environment';
+
+
+
 
 fetch('/assets/config.json')
   .then(response => response.json())
   .then(config => {
     environment.baseApiUrl = config.apiUrl;
+    console.log("baseUrl : ",config.apiUrl);
 
     platformBrowserDynamic().bootstrapModule(AppModule)
       .catch(err => console.error(err));
