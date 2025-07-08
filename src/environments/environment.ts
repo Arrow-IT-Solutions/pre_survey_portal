5b38defcafee357e35b43de4bcf1807ccd46515a
Nour <noursaleem@arrowitjo.com>
2025-07-08 13:13:48 +0300

    responsive notes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseApiUrl: "https://localhost:50275"
+  baseApiUrl: "https://survey-api-dev.apps-jo.com"
 };
 
 //https://survey-api-dev.apps-jo.com
